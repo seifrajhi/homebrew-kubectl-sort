de0fe8ed5ebd415f990b4672dfcf70f4669d6ce5
saifeddine Rajhi <rajhiseif@gmail.com>
2023-06-02 11:50:51 +0000

    kubectl-sort: update 1.0.0 bottle.

--- Formula/kubectl-sort.rb
+++ Formula/kubectl-sort.rb
@@ -5,13 +5,9 @@ class KubectlSort < Formula
   sha256 "8c1fb800c78e2f483c4791a1b76e8a4020acbd18e5ea142f0a800107a173f33b"
   license "MIT"
   bottle do
-    sha256 arm64_ventura:  "a6f7dd653b6c5904629effbfff359da0aa9903786b38abc0840c595e281a278e"
-    sha256 arm64_monterey: "270d1f7c56978757922b246dfd8bccead979c3d30a8e95d77a7b7b644050e6cd"
-    sha256 arm64_big_sur:  "13a7123b4e99f4bd574ec38e0a48d80ae6bbc36527cf9e158679f045d21160f4"
-    sha256 ventura:        "59844694b2bb56614623c8ede1679689fa32e8becf296108716de31ff3db6807"
-    sha256 monterey:       "e86210ffc0380bf4ccff3e3081e4dbd9c9ee3c2f72574d41498a817050f1ef86"
-    sha256 big_sur:        "af625209f6474e67aa4cc911c972425631d4e7ef343235b25bdf8ef11b761303"
-    sha256 x86_64_linux:   "e2df7e7b96ec43663841c3dfc8bc28d15087d39be147fde3d0dd1ba564be7a95"
+    root_url "https://github.com/seifrajhi/homebrew-kubectl-sort/releases/download/kubectl-sort-1.0.0"
+    sha256 cellar: :any_skip_relocation, ventura:      "66ebc643de49dcd2b6a5dd8766d5572611cd4b313ff10e16a0f031116170aeb8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0060ba0319276ea214a783a186f6d7ca0214d723923e84f6f630c350fda800c4"
   end
   def install
     bin.install "sort.py" => "sort"
