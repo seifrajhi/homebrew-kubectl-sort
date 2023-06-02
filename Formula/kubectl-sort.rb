37e98db94d054e0c49bd947abea67956ae097d71
seifrajhi <rajhiseif@gmail.com>
2023-06-02 16:10:10 +0200

    kubectl-sort 1.0.0 (new formula)

--- Formula/kubectl-sort.rb
+++ Formula/kubectl-sort.rb
@@ -10,7 +10,7 @@ class KubectlSort < Formula
     sha256 cellar: :any_skip_relocation, ventura:      "5fdca6ba7d615344bb9abd14715a1b974444fac285ae3735c2813d89a087fcc7"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "a365ef4c5ac015dfd1d2bda386eda196ad2bf0e1d4a8664c1d1f3f05572168fc"
   end
-  depends_on "python"
+  depends_on :python
   def install
     inreplace "sort.py", "#!/usr/bin/env python", "#!/usr/bin/env python3"
     bin.install "sort.py" => "kubectl-sort"
